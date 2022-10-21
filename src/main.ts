65afde2a675e6b7f20d44583726518edf2274197
Steven Higgan <stevenh@mad.geek.nz>
2022-10-22 01:38:02 +1300

    moving stuff about the new folder structure

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import './app.css'
-import App from './App.svelte'
+import './styles/app.css'
+import App from './pages/App.svelte'
 
 const app = new App({
   target: document.getElementById('app')!
