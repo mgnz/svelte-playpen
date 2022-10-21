acc326eea8eae4e2f3deef61569ad7d891ce0681
Steven Higgan <stevenh@mad.geek.nz>
2022-10-21 20:07:27 +1300

    added prettier config

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
-  let count: number = 0;
-  const increment = () => {
-    count += 1;
-  };
+	let count: number = 0;
+	const increment = () => {
+		count += 1;
+	};
 </script>
 
 <button on:click={increment}>
-  <h1>count is {count}</h1>
+	<h1>count is {count}</h1>
 </button>
 
 <style type="text/scss">
-  button {
-    h1 {
-      color: red;
-    }
-  }
+	button {
+		h1 {
+			color: yellow;
+		}
+	}
 </style>
