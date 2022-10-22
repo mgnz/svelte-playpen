fb3a3b2422da01f6af887232a306a0daa9fb4a38
Steven Higgan <stevenh@mad.geek.nz>
2022-10-22 23:29:33 +1300

    splitting up the legacy stuff so it doesnt mix in

--- src/pages/App.svelte
+++ src/pages/App.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import 'bootstrap/dist/css/bootstrap.min.css';
+	import 'bootstrap/dist/css/bootstrap.css';
 	import svelteLogo from '../assets/svelte.svg';
 	import Counter from '../lib/components/Counter.svelte';
 	import Header from '../lib/components/Header.svelte';
@@ -7,45 +7,50 @@
 
 <Header />
 
-<main>
-	<div>
-		<a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-			<img src="/vite.svg" class="logo" alt="Vite Logo" />
-		</a>
-		<a href="https://svelte.dev" target="_blank" rel="noreferrer">
-			<img src="{svelteLogo}" class="logo svelte" alt="Svelte Logo" />
-		</a>
-	</div>
-	<h1>Vite + Svelte</h1>
+<main id="main">
+	<legacy-main class="legacy-main">
+		<div>
+			<a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+				<img src="/vite.svg" class="logo" alt="Vite Logo" />
+			</a>
+			<a href="https://svelte.dev" target="_blank" rel="noreferrer">
+				<img src="{svelteLogo}" class="logo svelte" alt="Svelte Logo" />
+			</a>
+		</div>
+		<h1>Vite + Svelte</h1>
 
-	<div class="card">
-		<Counter />
-	</div>
+		<div class="card">
+			<Counter />
+		</div>
 
-	<p>
-		Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the
-		official Svelte app framework powered by Vite!
-	</p>
+		<p>
+			Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the
+			official Svelte app framework powered by Vite!
+		</p>
 
-	<p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
+		<p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
+	</legacy-main>
 </main>
 
 <style lang="scss">
-	.logo {
-		height: 6em;
-		padding: 1.5em;
-		will-change: filter;
+	// legacy bollocks
+	.legacy-main {
+		.logo {
+			height: 6em;
+			padding: 1.5em;
+			will-change: filter;
 
-		&:hover {
-			filter: drop-shadow(0 0 2em #646cffaa);
+			&:hover {
+				filter: drop-shadow(0 0 2em #646cffaa);
+			}
+
+			&.svelte:hover {
+				filter: drop-shadow(0 0 2em #ff3e00aa);
+			}
 		}
 
-		&.svelte:hover {
-			filter: drop-shadow(0 0 2em #ff3e00aa);
+		.read-the-docs {
+			color: #888;
 		}
-	}
-
-	.read-the-docs {
-		color: #888;
 	}
 </style>
