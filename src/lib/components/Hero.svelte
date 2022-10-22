7055a9c5006f1ac21ef1eb810d237ec4aa8a0972
Steven Higgan <stevenh@mad.geek.nz>
2022-10-23 01:25:38 +1300

    adding hero

---
+++ src/lib/components/Hero.svelte
@@ -0,0 +1,90 @@
+<script lang="ts">
+	import heroBackground from '../../assets/hero-bg.jpg';
+</script>
+
+<section
+	id="hero"
+	class="d-flex flex-column justify-content-center align-items-center"
+	style="
+		width: 100%;
+		height: 100vh;
+		background: url('{heroBackground}') top center;
+		background-size: cover;
+		">
+	<!-- usage of inline style is naughty; but i have no idea how to pass the profile image down to the style -->
+	<div class="hero-container" data-aos="fade-in">
+		<h1>Alex Smith</h1>
+		<p>I'm <span class="typed" data-typed-items="Designer, Developer, Freelancer, Photographer"></span></p>
+	</div>
+</section>
+
+<style lang="scss">
+	@import '../../styles/app.scss';
+
+	#hero {
+		//width: 100%;
+		//height: 100vh;
+		//background: url('{heroBackground}') top center;
+		//background-size: cover;
+
+		&:before {
+			content: '';
+			background: rgba(5, 13, 24, 0.3);
+			position: absolute;
+			bottom: 0;
+			top: 0;
+			left: 0;
+			right: 0;
+			z-index: 1;
+		}
+
+		.hero-container {
+			position: relative;
+			z-index: 2;
+			min-width: 300px;
+		}
+
+		h1 {
+			margin: 0 0 10px 0;
+			font-size: 64px;
+			font-weight: 700;
+			line-height: 56px;
+			color: #fff;
+		}
+
+		p {
+			color: #fff;
+			margin-bottom: 50px;
+			font-size: 26px;
+			font-family: 'Poppins', sans-serif;
+
+			span {
+				color: #fff;
+				padding-bottom: 4px;
+				letter-spacing: 1px;
+				border-bottom: 3px solid #149ddd;
+			}
+		}
+	}
+
+	@media (min-width: $global-width-med) {
+		#hero {
+			background-attachment: fixed;
+		}
+	}
+
+	@media (max-width: $global-width-sm) {
+		#hero {
+			h1 {
+				font-size: 28px;
+				line-height: 36px;
+			}
+
+			// h2 {
+			// 	font-size: 18px;
+			// 	line-height: 24px;
+			// 	margin-bottom: 30px;
+			// }
+		}
+	}
+</style>
--- src/pages/App.svelte
+++ src/pages/App.svelte
@@ -3,11 +3,15 @@
 	import svelteLogo from '../assets/svelte.svg';
 	import Counter from '../lib/components/Counter.svelte';
 	import Header from '../lib/components/Header.svelte';
+	import Hero from '../lib/components/Hero.svelte';
 </script>
 
 <Header />
 
+<Hero />
+
 <main id="main">
+	<!--
 	<legacy-main class="legacy-main">
 		<div>
 			<a href="https://vitejs.dev" target="_blank" rel="noreferrer">
@@ -30,10 +34,12 @@
 
 		<p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
 	</legacy-main>
+	-->
 </main>
 
 <style lang="scss">
 	@import '../styles/app.scss';
+
 	#main {
 		margin-left: 300px;
 	}
@@ -44,24 +50,65 @@
 		}
 	}
 
-	// legacy bollocks
-	.legacy-main {
-		.logo {
-			height: 6em;
-			padding: 1.5em;
-			will-change: filter;
+	/*--------------------------------------------------------------
+	# Sections General
+	--------------------------------------------------------------*/
 
-			&:hover {
-				filter: drop-shadow(0 0 2em #646cffaa);
-			}
+	section {
+		padding: 60px 0;
+		overflow: hidden;
+	}
 
-			&.svelte:hover {
-				filter: drop-shadow(0 0 2em #ff3e00aa);
+	.section-bg {
+		background: #f5f8fd;
+	}
+
+	.section-title {
+		padding-bottom: 30px;
+
+		h2 {
+			font-size: 32px;
+			font-weight: bold;
+			margin-bottom: 20px;
+			padding-bottom: 20px;
+			position: relative;
+			color: #173b6c;
+
+			&::after {
+				content: '';
+				position: absolute;
+				display: block;
+				width: 50px;
+				height: 3px;
+				background: #149ddd;
+				bottom: 0;
+				left: 0;
 			}
 		}
 
-		.read-the-docs {
-			color: #888;
+		p {
+			margin-bottom: 0;
 		}
 	}
+
+	// legacy bollocks
+	// .legacy-main {
+	// 	.logo {
+	// 		height: 6em;
+	// 		padding: 1.5em;
+	// 		will-change: filter;
+
+	// 		&:hover {
+	// 			filter: drop-shadow(0 0 2em #646cffaa);
+	// 		}
+
+	// 		&.svelte:hover {
+	// 			filter: drop-shadow(0 0 2em #ff3e00aa);
+	// 		}
+	// 	}
+
+	// 	.read-the-docs {
+	// 		color: #888;
+	// 	}
+	// }
 </style>
