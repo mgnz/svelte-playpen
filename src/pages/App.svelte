0a807453b56992e350bc4778c0940a56f2642398
Steven Higgan <stevenh@mad.geek.nz>
2022-10-23 16:14:50 +1300

    initalising aos and pure-counter in app

--- src/pages/App.svelte
+++ src/pages/App.svelte
@@ -1,10 +1,34 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+
 	import 'bootstrap/dist/css/bootstrap.css';
 	import MobileToggle from '../lib/components/MobileToggle.svelte';
 	import Header from '../lib/components/Header.svelte';
 	import Hero from '../lib/components/Hero.svelte';
 	import AboutSection from '../lib/components/AboutSection.svelte';
 	import FactsSection from '../lib/components/FactsSection.svelte';
+
+	import AOS from 'aos';
+	import 'aos/dist/aos.css'; // You can also use <link> for styles
+
+	import PureCounter from '@srexi/purecounterjs';
+
+	onMount(async () => {
+		/**
+		 * Animation on scroll
+		 */
+		AOS.init({
+			duration: 1000,
+			easing: 'ease-in-out',
+			once: true,
+			mirror: false,
+		});
+
+		/**
+		 * Initiate Pure Counter
+		 */
+		new PureCounter();
+	});
 </script>
 
 <MobileToggle />
