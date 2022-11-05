6c6202ae27400b966cf3198fc3ae4f173ac4fbdd
Steven Higgan <stevenh@mad.geek.nz>
2022-11-06 12:43:45 +1300

    wip stubing out contactsection

---
+++ src/lib/components/ContactSection.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import 'bootstrap/dist/css/bootstrap.css';
+
+	import ContactSectionBio from './ContactSectionBio.svelte';
+	import ContactSectionContact from './ContactSectionContact.svelte';
+</script>
+
+<style lang="scss">
+</style>
